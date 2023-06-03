f3f9971106cd24073c805674c1875fbb356bbbbe
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-06-03 21:19:11 +0000

    Initial changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
-
-  def destroy
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new"
+      end
+    else
+    flash["notice"] = "Nope."
+    redirect_to "/sessions/new"
   end
 end
-  
+
+def destroy
+  session["user_id"] = nil
+  flash["notice"] = "Goodbye."
+  redirect_to "/login"
+end
+end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
-    redirect_to "/"
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
+    if User.find_by({"email" => params["user"]["email"]}) != nil
+      flash["notice"] = "Please use existing log-in."
+      redirect_to "/sessions/new"
+    else
+      @user.save
+      redirect_to "/posts"
+    end 
   end
 end
